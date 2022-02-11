cc754c9e1330b4b808f637753075251be7719cb9
Galexanderg03 <galexanderg03@gmail.com>
2022-02-11 17:52:12 -0600

    Finale

--- src/lab4p2_gabrielgiron_12051024/Lab4P2_GabrielGiron_12051024.java
+++ src/lab4p2_gabrielgiron_12051024/Lab4P2_GabrielGiron_12051024.java
@@ -199,6 +199,9 @@ static ArrayList<Familia> Familias = new ArrayList();
         }
         else
         {
+            if(Apellido.equals("Capuleto"))
+                Familias.get(1).getPersonas().remove(0);
+            
             int pos = 0;
             int cent = 0;
             for (int i = 0; i < Familias.size(); i++) {
@@ -223,6 +226,42 @@ static ArrayList<Familia> Familias = new ArrayList();
                 {
                     int r1 = R.nextInt(Familias.get(pos).getPersonas().size());
                     int r2 = R.nextInt(Familias.get(0).getPersonas().size());
+                    Persona P1 = Familias.get(pos).getPersonas().get(r1);
+                    Persona P2 = Familias.get(0).getPersonas().get(r2);
+                    int Hp1 = P1.getHP();
+                    int Hp2 = P2.getHP();
+                    
+                    while(P1.getHP() > 0 && P2.getHP() > 0)
+                    {
+                        System.out.println(P1.getNombre()+ " " + P1.getApellido()+ " Atacara: " + P2.getNombre() + " " + P2.getApellido());
+                        Pelear(P1,P2,pos,r1);
+                        System.out.println(P2.getNombre()+ " " + P2.getApellido()+ " Tiene: "+P2.getHP()+" Vida");
+                        if(P2.getHP() <= 0)
+                        {       
+                            if(P2 instanceof SuperGranjero)
+                            {
+                                System.out.println(P2.getNombre()+" "+P2.getApellido()+" Ha Muerto, Has perdido el juego");
+                                System.exit(0);
+                            }
+                            else
+                            {
+                                System.out.println(P2.getNombre()+" "+P2.getApellido()+" Ha Muerto");
+                                Familias.get(0).getPersonas().remove(r2);
+                                P1.setHP(Hp1);
+                                break;
+                            }
+                        }
+                        System.out.println(P2.getNombre()+ " " + P2.getApellido()+ " Atacara: " + P1.getNombre() + " " + P1.getApellido());
+                        Pelear(P2,P1,0,r2);
+                        System.out.println(P1.getNombre()+ " " + P1.getApellido()+ " Tiene: "+P1.getHP()+" Vida");
+                        if(P1.getHP() <= 0)
+                        {
+                            System.out.println(P1.getNombre()+" "+P1.getApellido()+" Ha Muerto");
+                            Familias.get(pos).getPersonas().remove(r1);
+                            P2.setHP(Hp2);
+                            break;
+                        }
+                    }
                 }
             }
         }
@@ -242,7 +281,7 @@ static ArrayList<Familia> Familias = new ArrayList();
         Persona J = new Normales("Julieta","Capuleto",17,50);
         Persona P = new Pacifista("Julio","Capuleto",20,100);
         Persona E = new Explosivo("Abraham","Capuleto",20,300);
-        Persona B = new Herrero(300,"Bryan","Alvarez",20,400);
+        Persona B = new Herrero(300,"Bryan","Alvarez",20,200);
         
         //crear Add al ArrayList;
         Familias.add(M);
@@ -256,6 +295,150 @@ static ArrayList<Familia> Familias = new ArrayList();
         C.getPersonas().add(E);
         A.getPersonas().add(B);
     }
+    
+    public static void Pelear(Persona P1, Persona P2, int pos, int r)
+    {
+        int Fallar = 1+R.nextInt(99);
+        if(P1 instanceof Normales)
+        {
+            int Hp1 = P1.getHP();
+            int Atk1 = ((Normales)Familias.get(pos).getPersonas().get(r)).getAtk();
+            int Hp2 = P2.getHP();
+            if(P2 instanceof Pacifista)
+            {
+                Hp2 = Hp2 - ((Atk1 * 5)/100);
+                P2.setHP(Hp2);
+            }
+            else
+            {
+                Hp2 = Hp2 - Atk1;
+                P2.setHP(Hp2);
+            }
+            P2.setHP(Hp2);
+        }
+        
+        if(P1 instanceof Pacifista)
+        {
+            System.out.println(P1.getNombre() + " " +P1.getApellido()+" Dice su Discurso de Paz.........pero este no funciona");
+        }
+        
+        if(P1 instanceof Herrero)
+        {
+            int Hp1 = P1.getHP();
+            int Atk1 = ((Herrero)Familias.get(pos).getPersonas().get(r)).getAtk();
+            int Hp2 = P2.getHP();
+            if(Fallar <= 10)
+            {
+                System.out.println(P1.getNombre() + " " +P1.getApellido()+" Fallo el Ataque");
+            }
+            else
+            {
+                if(P2 instanceof Pacifista)
+                {
+                    Hp2 = Hp2 - ((Atk1 * 5)/100);
+                    P2.setHP(Hp2);
+                }
+                else if(P2 instanceof Agronomo)
+                {
+                    Hp2 = Hp2 - ((Atk1 * 10)/100);
+                    P2.setHP(Hp2);
+                }
+                else
+                {
+                    Hp2 = Hp2 - Atk1;
+                    P2.setHP(Hp2);
+                }
+            }
+        }
+        
+        if(P1 instanceof Agronomo)
+        {
+            int Hp1 = P1.getHP();
+            int Atk1 = ((Agronomo)Familias.get(pos).getPersonas().get(r)).getAtk();
+            int Hp2 = P2.getHP();
+            if(Fallar <= 5)
+            {
+                System.out.println(P1.getNombre() + " " +P1.getApellido()+" Fallo el Ataque");
+            }
+            else
+            {
+                if(P2 instanceof Pacifista)
+                {
+                    Hp2 = Hp2 - ((Atk1 * 5)/100);
+                    P2.setHP(Hp2);
+                }
+                else if(P2 instanceof Normales)
+                {
+                    Hp2 = Hp2 - ((Atk1 * 10)/100);
+                    P2.setHP(Hp2);
+                }
+                else
+                {
+                    Hp2 = Hp2 - Atk1;
+                    P2.setHP(Hp2);
+                }
+            }
+        }
+        
+        if(P1 instanceof Explosivo)
+        {
+            int Hp1 = P1.getHP();
+            int Atk1 = ((Explosivo)Familias.get(pos).getPersonas().get(r)).getAtk();
+            int Hp2 = P2.getHP();
+            if(Fallar <= 15)
+            {
+                System.out.println(P1.getNombre() + " " +P1.getApellido()+" Fallo el Ataque");
+            }
+            else
+            {
+                if(P2 instanceof Herrero)
+                {
+                    Hp2 = Hp2 - ((Atk1 * 5)/100);
+                    P2.setHP(Hp2);
+                }
+                else if(P2 instanceof Agronomo)
+                {
+                    Hp2 = Hp2 - ((Atk1 * 10)/100);
+                    P2.setHP(Hp2);
+                }
+                else
+                {
+                    Hp2 = Hp2 - Atk1;
+                    P2.setHP(Hp2);
+                }
+            }
+        }
+        if(P1 instanceof SuperGranjero)
+        {
+            int Hp1 = P1.getHP();
+            int Atk1 = ((SuperGranjero)Familias.get(pos).getPersonas().get(r)).getAtk();
+            int Hp2 = P2.getHP();
+            if(Fallar <= 15)
+            {
+                System.out.println(P1.getNombre() + " " +P1.getApellido()+" Fallo el Ataque");
+            }
+            else
+            {
+                if(P2 instanceof Herrero)
+                {
+                    Hp2 = Hp2 - ((Atk1 * 10)/100);
+                    P2.setHP(Hp2);
+                }
+                else if(P2 instanceof Explosivo)
+                {
+                    Hp2 = Hp2 - ((Atk1 * 15)/100);
+                    P2.setHP(Hp2);
+                }
+                else
+                {
+                    Hp2 = Hp2 - Atk1;
+                    P2.setHP(Hp2);
+                }
+            }
+        }
+        
+    }
+    
     public static void print()
     {
         System.out.println("Lista Familias");
